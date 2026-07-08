82822ca2f5c5ae37efc4bcfc1d9c1ce8da080010
Ashkanmir11 <ashkan110mir@gmail.com>
2026-07-08 13:40:48 +0330

    create domain project

---
+++ OnlineExam.Domain/Entities/ClassRoom.cs
@@ -0,0 +1,15 @@
+﻿using OnlineExam.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Domain.Entities
+{
+    public class ClassRoom : BaseDTO
+    {
+        public string? ClassName { get; set; }
+        public string? TeacherId { get; set; }
+    }
+}
---
+++ OnlineExam.Domain/Entities/ClassRoomUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Domain.Entities
+{
+    public class ClassRoomMembers
+    {
+        public string UserId { get; set; }
+        public int ClassRomeId { get; set; }
+    }
+}
