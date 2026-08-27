85dd85268fb302d5a5c8c7ecbb9fea0adc238766
Ashkanmir11 <ashkan110mir@gmail.com>
2026-08-27 16:08:13 +0330

    add test project with shoudly-add very simple test

---
+++ OnlineExam.test/Handlers/ClassRoomHandler.cs
@@ -0,0 +1,21 @@
+﻿using OnlineExam.Test.Models.ClassRoom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Test.Handlers
+{
+    public class ClassRoomHandler
+    {
+        public bool Craete(CreateClassRoomDTO createClassRoomDTO)
+        {
+            if (createClassRoomDTO.ClassName != null && createClassRoomDTO.ClassName.Length < 1)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ OnlineExam.test/Models/ClassRoom/CreateClassRoomDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineExam.Test.Models.ClassRoom
+{
+    public class CreateClassRoomDTO
+    {
+        public string? ClassName { get; set; }
+    }
+}
